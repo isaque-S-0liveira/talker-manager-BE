07ea5dbb0824ec54e896ff935e83cfb5519dc008
Isaque <zazac3179@gmail.com>
2023-04-14 17:41:58 -0300

    req 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const fs = require('fs').promises;
 const app = express();
 app.use(express.json());
 
@@ -14,3 +14,17 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+
+const readifile = async () => {
+  const response = await fs.readFile('src/talker.json', "utf-8");
+  return JSON.parse(response);
+}
+
+app.get('/talker', async (req, res) => {
+  const  persons = await readifile();
+  if (persons.length === 0) {
+    return res.status(200).send([])
+  } 
+  return  res.status(200).send(persons);
+})
